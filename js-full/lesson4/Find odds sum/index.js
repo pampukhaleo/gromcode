d4468c9ef70d9021b5e9282b0504b01f3611bf2e
pampukhaleo <lgpampukha@gmail.com>
2022-07-07 23:05:37 +0300

    loops

---
+++ js-full/lesson4/Find odds sum/index.js
@@ -0,0 +1,14 @@
+let sum = 0;
+
+for (let i = 0; i <= 100; i++) {
+  if (i % 2 === 1) {
+    console.log("Found");
+    sum += i;
+  }
+}
+
+if (sum * 5 > 5000) {
+  console.log("Bigger");
+} else {
+  console.log("Smaller or equal");
+}
---
+++ js-full/lesson4/numbers-string/index.js
@@ -0,0 +1,8 @@
+const n = 10;
+let sum = "";
+
+for (let i = 1; i <= n; i++) {
+  sum += i;
+}
+
+console.log(sum)
