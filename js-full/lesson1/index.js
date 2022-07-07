3e495b6ff989e968986f8e4814e31289c28442ea
pampukhaleo <lgpampukha@gmail.com>
2022-07-07 21:44:03 +0300

    loops multiply till 5

--- js-full/lesson1/index.js
+++ js-full/lesson1/index.js
@@ -1,25 +1,10 @@
-/* eslint-disable */
+'use strict'
 
-/* Этот код менять не нужно */
-const defaultUserName = 'anonymus';
-const userName = 'John';
-const anotherUser = null;
-const isAdmin = false;
-const message = 'text';
-const anotherMessage = 'text as well';
-const isLoggedIn = Boolean(userName);
-
-/* Раскоментируй код ниже, используя правильные операторы после знака присваивания */
-
-const activeUser = anotherUser || defaultUserName;
-const hasAccess = isAdmin || isLoggedIn;
-const isTruthy = message < anotherMessage;
-const isFalsy = !isLoggedIn;
-const isNotTrue = typeof message == 'number';
-const isTrue = typeof message > 'boolean';
-console.log(activeUser);
-console.log(hasAccess);
-console.log(isTruthy);
-console.log(isFalsy);
-console.log(isNotTrue);
-console.log(isTrue);
+for (let i=1; i<=5; i++) {
+  console.log(`
+  => ${i} <=
+  `)
+  for (let j=1; j<=9; j++) {
+    console.log(`${i} x ${j} = ${i * j}`)
+  }
+}
