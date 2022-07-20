df5d3e7fdc7c7e0b5d3c933fc3f0d593a102efc8
Leo Pampukha <lgpampukha@gmail.com>
2022-07-20 15:26:36 +0300

    more string 2

--- js-full/lesson12/clean-transaction-list/index.js
+++ js-full/lesson12/clean-transaction-list/index.js
@@ -1,7 +1,19 @@
-const cleanTransactionsList = arr => {
-  return arr
-      .filter(el => el / 0)
-      .map(el => `$${Number(el.trim()).toFixed(2)}`)
-};
+// Learn requirement
+// Create step by step algorithm. Input Output
+// Write draft solution & do testing
+// Refactoring & final testing
 
-console.log(cleanTransactionsList(['1', '  2  ', '3.4   ', '1 dollar', '17', 'qweqwe']));
+//input array
+//output new array
+
+// find elements without letters
+// if element has spaces - cut it of
+// final element has to be string with $ and 2 digits after dot
+
+const cleanTransactionsList = arr => arr
+    .filter(el => el / 0)
+    .map(el => `$${Number(el).toFixed(2)}`);
+
+const array = ['1', '  2  ', '3.4   ', '1 dollar', '17', 'qweqwe', 2, 7, undefined, null];
+console.log('array', array)
+console.log('function', cleanTransactionsList(array));
