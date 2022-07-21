1f42a93ebb79da40621cbc0ebc8d375601a05f29
pampukhaleo <lgpampukha@gmail.com>
2022-07-21 17:52:13 +0300

    jest

---
+++ js-full/lesson13/first-test/index.test.js
@@ -0,0 +1,16 @@
+it('17 is 17', () => {
+  expect(17).toEqual(17);
+});
+
+it('18 is not 17', () => {
+  expect(18).not.toEqual(17);
+});
+
+const getEvenNumbers = arr =>
+  arr.filter(num => num % 2 === 0);
+
+it('should get Even numbers', () => {
+  const result = getEvenNumbers([1, 2, 3, 4]);
+
+  expect(result).toEqual([2, 4]);
+});
