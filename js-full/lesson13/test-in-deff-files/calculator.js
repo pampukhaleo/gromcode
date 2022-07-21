2860fb01c31b38691d5b3ba3d076bb6dd900a1b3
pampukhaleo <lgpampukha@gmail.com>
2022-07-21 19:21:56 +0300

    jest test diff files

---
+++ js-full/lesson13/test-in-deff-files/calculator.js
@@ -0,0 +1,34 @@
+// Learn requirement
+// Create step by step algorithm. Input Output
+// Write draft solution & do testing
+// Refactoring & final testing
+
+// input array
+// output new array
+
+// getSquaredArray
+// input array
+// output array
+// element * element
+// make it export from file
+
+// getOddNumbers
+// input array
+// output array
+// filter for Odd - 1,3,5
+// make it export from file
+
+// default export
+// input (num, num)
+// output sum of input numbers
+
+export const getSquaredArray = arr =>
+  arr.map(element => element * element);
+
+export const getOddNumbers = arr =>
+  arr.filter(number => number % 2 !== 0);
+
+export default (a, b) => a + b
+
+const array = [1, 2];
+console.log(getSquaredArray(array));
---
+++ js-full/lesson13/test-in-deff-files/calculator.test.js
@@ -0,0 +1,19 @@
+import getSum, { getSquaredArray, getOddNumbers } from "./calculator";
+
+it('should get squared numbers', () => {
+  const result = getSquaredArray([1, 2, 3, 4]);
+
+  expect(result).toEqual([1, 4, 9, 16]);
+});
+
+it('should get Odd numbers', () => {
+  const result = getOddNumbers([1, 2, 3, 4]);
+
+  expect(result).toEqual([1, 3]);
+});
+
+it('should get sum of numbers', () => {
+  const result = getSum(1, 2);
+
+  expect(result).toEqual(3);
+});
