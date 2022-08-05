82d0f3e8907a208f47e4656786db605604bef158
pampukhaleo <lgpampukha@gmail.com>
2022-08-05 22:18:20 +0300

    bind func

--- js-full/lesson17/bind-func/index.js
+++ js-full/lesson17/bind-func/index.js
@@ -1,13 +1,18 @@
-const bind = (func, context, arg1, arg2) => {
-  return func.call(context, arg1, arg2);
+const bind = (func, context, ...rest) => {
+  return function (...args) {
+    // return func.call(context, ...rest.concat(args))
+    return func.apply(context, rest.concat(args));
+  };
 };
+
+// https://www.youtube.com/watch?v=fJqYa3BuwaU
 
 const user = {
   name: 'Tom',
 };
 
-function showName(arg) {
-  console.log(`${arg} ${this.name}`);
+function showName(arg, arg2) {
+  console.log(`${arg} ${this.name} ${arg2}`);
 }
 
-bind(showName, user, 'Hi');
+bind(showName, user, 'Hi', 'man')();
