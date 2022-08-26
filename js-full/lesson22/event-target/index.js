54aeab9bca2d0ad529b7c20da8a32cf2f2a63883
Leo Pampukha <lgpampukha@gmail.com>
2022-08-26 15:25:35 +0300

    event.target

--- js-full/lesson22/event-target/index.js
+++ js-full/lesson22/event-target/index.js
@@ -1,3 +1,7 @@
 'use strict';
 
-
+const btn = document.querySelectorAll('.btn')
+const handleClick = (event) => {
+  console.log(event.target.textContent);
+}
+Array.from(btn).map(element => element.addEventListener('click', handleClick))
