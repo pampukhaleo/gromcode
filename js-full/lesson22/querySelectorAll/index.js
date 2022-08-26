14ab5de8a15973e640e2c7b4ad0eab8db5b3e2c1
Leo Pampukha <lgpampukha@gmail.com>
2022-08-26 15:46:42 +0300

    querySelectorAll

--- js-full/lesson22/querySelectorAll/index.js
+++ js-full/lesson22/querySelectorAll/index.js
@@ -1,6 +1,5 @@
 'use strict';
 
 const btn = document.querySelectorAll('.pagination__page');
-Array.from(btn).map(element =>
-  element.addEventListener('click', event => console.log(event.target.dataset.pageNumber)),
-);
+const handleClick = event => console.log(event.target.dataset.pageNumber);
+Array.from(btn).map(element => element.addEventListener('click', handleClick));
