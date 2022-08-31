2a0cbb1e08add989e42be0edd83fa48f2d5a9adb
Leo Pampukha <lgpampukha@gmail.com>
2022-08-31 14:41:52 +0300

    grinwitch time

--- js-full/lesson24/utc-time/index.js
+++ js-full/lesson24/utc-time/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const formatter = new Intl.DateTimeFormat('en', {
+export const formatter = new Intl.DateTimeFormat('en', {
   timeZone: 'UTC',
   hour: '2-digit',
   minute: '2-digit',
