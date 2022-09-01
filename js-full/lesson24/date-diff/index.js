a68409e2ac23bffd85c6b4e5f347869746df62af
Leo Pampukha <lgpampukha@gmail.com>
2022-09-01 11:35:16 +0300

    date diff

--- js-full/lesson24/date-diff/index.js
+++ js-full/lesson24/date-diff/index.js
@@ -1,27 +1,26 @@
 'use strict';
 
 export const getDiff = (startDate, endDate) => {
-
   // get total seconds between the times
-  let timeDiff = (endDate.getTime() - startDate.getTime()) / 1000;
+  let timeDiff = Math.abs(endDate - startDate) / 1000;
   console.log('seconds diff', timeDiff);
 
   // calculate (and subtract) whole days
   const daysDiff = Math.floor(timeDiff / (3600 * 24));
-  timeDiff -= daysDiff * 86400
+  timeDiff -= daysDiff * 86400;
   console.log('seconds diff after days subtract', timeDiff);
 
   // calculate (and subtract) whole hours
-  const hoursDiff = Math.floor(timeDiff / 3600);
+  const hoursDiff = Math.floor(timeDiff / 3600) % 24;
   timeDiff -= hoursDiff * 3600;
   console.log('seconds diff after hours subtract', timeDiff);
 
   // calculate (and subtract) whole minutes
-  const minutesDiff = Math.floor(timeDiff / 60);
+  const minutesDiff = Math.floor(timeDiff / 60) % 60;
   timeDiff -= minutesDiff * 60;
   console.log('seconds diff after minutes subtract', timeDiff);
 
-  // const secondsDiff = timeDiff % 60;
+  const secondsDiff = timeDiff % 60;
   console.log('seconds diff after minutes subtract', timeDiff);
 
   console.log('days diff', daysDiff);
@@ -29,7 +28,7 @@ export const getDiff = (startDate, endDate) => {
   console.log('min diff', minutesDiff);
   console.log('sec diff', timeDiff);
 
-  return `${daysDiff}d ${hoursDiff}h ${minutesDiff}m ${timeDiff}s`
-}
+  return `${daysDiff}d ${hoursDiff}h ${minutesDiff}m ${secondsDiff}s`;
+};
 
 // console.log(getDiff(new Date(2020, 2,1,10, 55, 50), new Date(2021, 1, 1, 11, 57, 55)))
