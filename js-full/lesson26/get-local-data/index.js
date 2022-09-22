e0b0ac8b7f09161936ae74a0f3e43ef3e139e12a
pampukhaleo <lgpampukha@gmail.com>
2022-09-22 20:53:07 +0300

    getLocalData

---
+++ js-full/lesson26/get-local-data/index.js
@@ -0,0 +1,10 @@
+export const getLocalStorageData = () => {
+  return Object.entries(localStorage)
+    .reduce((acc, [key, value]) => {
+      return {
+        ...acc,
+        [key]: JSON.parse(value),
+      }
+  }, {})
+}
+
