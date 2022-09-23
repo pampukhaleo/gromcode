872cc2a1472dd473079f2eee098e3fba9b3e13ac
pampukhaleo <lgpampukha@gmail.com>
2022-09-23 09:30:26 +0300

    try catch

--- js-full/lesson26/get-local-data/index.js
+++ js-full/lesson26/get-local-data/index.js
@@ -1,10 +1,15 @@
-localStorage.clear()
 export const getLocalStorageData = () => {
   return Object.entries(localStorage)
     .reduce((acc, [key, value]) => {
+      let newValue;
+      try {
+        newValue = JSON.parse(value)
+      } catch (e) {
+        newValue = value;
+      }
       return {
         ...acc,
-        [key]: JSON.parse(value),
+        [key]: newValue,
       }
   }, {})
 }
