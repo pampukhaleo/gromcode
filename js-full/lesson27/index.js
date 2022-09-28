ede6f8153c2dee779cf9fb2225698d64aac03ce7
Leo Pampukha <lgpampukha@gmail.com>
2022-09-28 14:43:10 +0300

    chaining

---
+++ js-full/lesson27/index.js
@@ -0,0 +1,27 @@
+const calc = value => {
+  let result = value;
+
+  const calculator = {
+    add(number) {
+      result += number;
+      return this;
+    },
+    subtract(number) {
+      result -= number;
+      return this;
+    },
+    mult(number) {
+      result *= number;
+      return this;
+    },
+    div(number) {
+      result /= number;
+      return this;
+    },
+    result() {
+      return result;
+    },
+  };
+
+  return calculator;
+};
