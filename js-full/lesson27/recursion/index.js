c599c9e82dfdab3dd62da6cfe82dc9814a82fa37
Leo Pampukha <lgpampukha@gmail.com>
2022-09-28 15:05:53 +0300

    recursion

---
+++ js-full/lesson27/recursion/index.js
@@ -0,0 +1,23 @@
+const favorites = ['id-2'];
+const tree = {
+  id: 'id-1',
+  name: 'Products',
+  nodes: [
+    {
+      id: 'id-2',
+      name: 'Food',
+      nodes: [],
+    },
+  ],
+};
+
+export const markFavorites = (tree, favorites) => {
+  const isFavorite = favorites.includes(tree.id);
+
+  console.log(tree.nodes);
+  return {
+    ...tree,
+    isFavorite,
+    nodes: tree.nodes.map(childNode => markFavorites(childNode, favorites)),
+  };
+};
