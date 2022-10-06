d1602244f9e0fcb032b3daa984cc6225354814a9
Leo Pampukha <lgpampukha@gmail.com>
2022-10-06 15:40:26 +0300

    assyncSum

--- js-full/lesson32/asyncSum/index.js
+++ js-full/lesson32/asyncSum/index.js
@@ -9,11 +9,11 @@ const asyncNumber1 = numberDelay(1, 1000);
 const asyncNumber2 = numberDelay(2, 2000);
 const asyncNumber3 = numberDelay(3, 3000);
 
-const numSum = numbers => numbers.reduce((acc, num) => acc + num, 0);
+const numSum = numbers => numbers.filter(value => !isNaN(value)).reduce((acc, num) => acc + num, 0);
 
-const asyncSum = (...asyncNumbers) =>
+export const asyncSum = (...asyncNumbers) =>
   Promise.all(asyncNumbers)
     .then(numbers => numSum(numbers))
     .catch(() => Promise.reject(new Error(`Can't calculate`)));
 
-asyncSum(asyncNumber1, new Error(), asyncNumber3).then(res => console.log(res));
+// asyncSum(asyncNumber1, new Error(), asyncNumber3).then(res => console.log(res));
