856a473221ea73326c006967e0532ff1406d7a07
pampukhaleo <lgpampukha@gmail.com>
2022-10-13 20:30:25 +0300

    try get array

--- js-full/lesson36/getUserBlog/index.js
+++ js-full/lesson36/getUserBlog/index.js
@@ -5,17 +5,21 @@
 
 export const getUsersBlogs = async users => {
   // put your code here
-  const blogList = users.map(async userName => {
-    const response = await fetch(`https://api.github.com/users/${userName}`);
-    const { blog } = await response.json();
-    return blog;
-  });
+  try {
+    const blogList = users.map(async userName => {
+      const response = await fetch(`https://api.github.com/users/${userName}`);
+      const { blog } = await response.json();
 
-  return blogList;
+      return blog;
+    });
+    return blogList;
+  } catch (e) {
+    throw new Error(e);
+  }
 };
 
 // getUsersBlogs('facebook').then(data => console.log(data));
 
 // examples
 // getUsersBlogs(['google', 'facebook', 'reactjs']).then(linksList => console.log(linksList)); // ==> ["https://opensource.google/", "https://opensource.fb.com", "https://reactjs.org"]
-// getUsersBlogs(['microsoft']).then(linksList => console.log('li', linksList)); // ==> ["https://opensource.microsoft.com"]
+// getUsersBlogs(['microsoft']).then(linksList => console.log(linksList)); // ==> ["https://opensource.microsoft.com"]
