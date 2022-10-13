1641417de70e4cd816c8012bea26ececcdf539e7
Leo Pampukha <lgpampukha@gmail.com>
2022-10-13 16:50:26 +0300

    getuserBlog

--- js-full/lesson36/getUserAsyncAwait/index.js
+++ js-full/lesson36/getUserAsyncAwait/index.js
@@ -2,12 +2,10 @@ export const fetchUser = async userId => {
   // put your code here
   try {
     const response = await fetch(`https://api.github.com/users/${userId}`);
-    // console.log(response);
     if (response.status === 404) {
       return null;
     }
     const userData = await response.json();
-    console.log(userData);
     return userData;
   } catch (err) {
     throw new Error('Failed to get user data');
---
+++ js-full/lesson36/getUserBlog/index.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[]} users
+ * @return {promise}
+ */
+
+export const getUsersBlogs = async users => {
+  // put your code here
+  const blogList = users.map(async userName => {
+    const response = await fetch(`https://api.github.com/users/${userName}`);
+    const userData = await response.json();
+    return userData.blog;
+  });
+
+  return blogList;
+};
+
+// getUsersBlogs('facebook').then(data => console.log(data));
+
+// examples
+// getUsersBlogs(['google', 'facebook', 'reactjs']).then(linksList => console.log(linksList)); // ==> ["https://opensource.google/", "https://opensource.fb.com", "https://reactjs.org"]
+// getUsersBlogs(['microsoft']).then(linksList => console.log(linksList)); // ==> ["https://opensource.microsoft.com"]
