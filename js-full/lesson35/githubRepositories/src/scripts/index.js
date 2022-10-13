3c828f1abe21bf0a9e3ea885acc480d65a3d19bb
Leo Pampukha <lgpampukha@gmail.com>
2022-10-13 13:14:33 +0300

    github repos

--- js-full/lesson35/githubRepositories/src/scripts/index.js
+++ js-full/lesson35/githubRepositories/src/scripts/index.js
@@ -1,5 +1,10 @@
-import { initHandlers } from './initHandlers.js';
 import { renderUserData } from './renderUserData.js';
+import { fetchReposData, fetchUserData } from './usersGateway.js';
+import { renderReposData } from './renderRepos.js';
+import { hideSpinner, showSpinner } from './spinner.js';
+import { clearData } from './clearData.js';
+
+const inputElem = document.querySelector('.name-form__input');
 
 const defaultUser = {
   avatar_url: 'https://avatars3.githubusercontent.com/u10001',
@@ -7,8 +12,21 @@ const defaultUser = {
   location: '',
 };
 
-renderUserData(defaultUser);
+export const onUserSearch = () => {
+  const userName = inputElem.value;
+  clearData();
+  showSpinner();
+  fetchUserData(userName)
+    .then(data => {
+      renderUserData(data);
+      return data.repos_url;
+    })
+    .then(url => fetchReposData(url))
+    .then(reposList => renderReposData(reposList))
+    .catch(err => alert(err.message))
+    .finally(() => hideSpinner());
+};
 
-window.addEventListener('DOMContentLoaded', () => {
-  initHandlers();
-});
+renderUserData(defaultUser);
+const showBtnElem = document.querySelector('.name-form__btn');
+showBtnElem.addEventListener('click', onUserSearch);
--- js-full/lesson35/githubRepositories/src/scripts/initHandlers.js
+++
@@ -1,6 +0,0 @@
-import { onUserSearch } from './onUserSearch.js';
-
-export const initHandlers = () => {
-  const showBtnElem = document.querySelector('.name-form__btn');
-  showBtnElem.addEventListener('click', onUserSearch);
-};
