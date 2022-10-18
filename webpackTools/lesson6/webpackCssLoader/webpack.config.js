fadc5706667e3126e89f0f9b43ab439df4692d1b
pampukhaleo <lgpampukha@gmail.com>
2022-10-18 21:29:59 +0300

    webpack@4 css-loader

--- webpackTools/lesson6/webpackCssLoader/webpack.config.js
+++ webpackTools/lesson6/webpackCssLoader/webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
           {
             loader: 'url-loader',
             options: {
-              limit: 8000,
+              limit: 8192,
               name: '[name].[ext]',
               outPath: 'images',
             },
