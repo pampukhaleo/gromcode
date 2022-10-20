002ac47dea54d2527d18f0bbd0406ebb54026e7c
pampukhaleo <lgpampukha@gmail.com>
2022-10-20 23:36:22 +0300

    jsx with webpack

---
+++ react/lesson1/JSX/src/index.js
@@ -0,0 +1,14 @@
+import './styles.css';
+
+const rootElement = document.querySelector('#root');
+
+const jsxElement = (
+  <div className='greeting'>
+    <div className='greeting__title'>Hello, world!</div>
+    <div className='greeting__text'>I'm learning React</div>
+  </div>
+)
+
+console.log(jsxElement)
+
+ReactDOM.render(jsxElement, rootElement);
---
+++ react/lesson1/JSX/webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'review_build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /.s?css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /.(jpg|png)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: '[name].[ext]',
+              outputPath: 'images',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
