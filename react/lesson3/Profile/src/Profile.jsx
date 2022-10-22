ef27fd7c715f9c823172fcc577f77e952203cbef
pampukhaleo <lgpampukha@gmail.com>
2022-10-22 20:42:37 +0300

    Profile

---
+++ react/lesson3/Profile/src/Profile.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import moment from "moment";
+import './profile.scss'
+
+const formatDate = date => moment(date).format("DD MMM YY");
+
+const Profile = (props) => {
+  return (
+    <div className="profile">
+      <div className='profile__name'>
+        {props.userData.firstName} {props.userData.lastName}
+      </div>
+      <div className='profile__birth'>
+        Was born {formatDate(props.userData.birthDate)} in {props.userData.birthPlace}
+      </div>
+    </div>
+  )
+}
+
+export default Profile;
---
+++ react/lesson3/Profile/src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Profile from "./Profile.jsx";
+
+const rootElement = document.querySelector('#root');
+
+const userData = {
+  firstName: 'James',
+  lastName: 'Bond',
+  birthDate: '1991-01-17T11:11:11.819Z',
+  birthPlace: 'London',
+};
+
+const App = () => <Profile userData={userData} />
+
+ReactDOM.render(<App />, rootElement);
