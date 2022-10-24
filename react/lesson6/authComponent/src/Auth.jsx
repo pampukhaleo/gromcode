cea9720ba34c4ff1ba2664d2772975cb7cd555b1
pampukhaleo <lgpampukha@gmail.com>
2022-10-24 19:03:54 +0300

    auth comp

---
+++ react/lesson6/authComponent/src/Auth.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Login from "./Login.jsx";
+import Logout from "./Logout.jsx";
+import Spinner from "./Spinner.jsx";
+
+class Auth extends React.Component {
+  state = {
+    isOnline: false,
+    isLoading: false
+  }
+
+  handleLogout = () => {
+    this.setState({
+      isOnline: false
+    })
+  }
+
+  handleLogin = () => {
+    this.setState({
+      isLoading: true
+    })
+    setTimeout(() => {
+      this.setState({
+        isLoading: false,
+        isOnline: true
+      })
+    }, 2000)
+
+  }
+
+  render() {
+    return (
+      this.state.isLoading
+        ? <Spinner size={'50px'}/>
+        : this.state.isOnline
+          ? <Logout onLogout={this.handleLogout} />
+          : <Login onLogin={this.handleLogin} />
+    )
+  }
+}
+
+export default Auth;
