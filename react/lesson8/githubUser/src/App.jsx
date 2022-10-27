78eac3872f6624832e54127d8ea9fc40763273f0
pampukhaleo <lgpampukha@gmail.com>
2022-10-27 23:50:42 +0300

    github user

---
+++ react/lesson8/githubUser/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import User from "./User.jsx";
+
+const App = () => {
+  return (
+    <User userId={'pampukha'}/>
+  )
+}
+
+export default App;
---
+++ react/lesson8/githubUser/src/User.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+class User extends React.Component {
+  state = {
+    user: null
+  }
+
+  componentDidMount() {
+    fetch(`https://api.github.com/users/${this.props.userId}`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({user: data})
+      })
+  }
+
+  render() {
+    if (!this.state.user) {
+      return null
+    }
+    return (
+      <div className="user">
+        <img
+          alt="User Avatar"
+          src={this.state.user.avatar_url}
+          className="user__avatar"
+        />
+          <div className="user__info">
+            <span className="user__name">{this.state.user.name}</span>
+            <span className="user__location">{this.state.user.location}</span>
+          </div>
+      </div>
+    )
+  }
+}
+
+export default User;
