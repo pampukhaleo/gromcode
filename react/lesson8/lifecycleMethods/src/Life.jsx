e9ac08c5d6dabc45bcadfc63eae17c60aca40524
pampukhaleo <lgpampukha@gmail.com>
2022-10-27 14:07:00 +0300

    changes

--- react/lesson8/lifecycleMethods/src/Life.jsx
+++ react/lesson8/lifecycleMethods/src/Life.jsx
@@ -5,7 +5,7 @@ class Life extends React.Component {
     super(props);
     console.log('constructor: good place to create state')
     this.state = {
-      test: null
+      number: Math.round(Math.random() * 100)
     }
   }
 
@@ -15,21 +15,47 @@ class Life extends React.Component {
 
   shouldComponentUpdate(nextProps, nextState) {
     console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render')
+    return true
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if (prevProps !== this.state.props) {
-      console.log('componentDidUpdate(prevProps, prevState): some updates based on new props')
-    }
+    console.log('componentDidUpdate(prevProps, prevState): some updates based on new props')
   }
 
   componentWillUnmount() {
     console.log('componentWillUnmount(): cleanup before DOM related to component will be removed')
   }
 
+  show = () => {
+    this.setState({
+      visible: true
+    })
+  }
+
+  hide = () => {
+    this.setState({
+      visible: false
+    })
+  }
+
+  update = () => {
+    this.setState({
+      number: Math.round(Math.random() * 100)
+    })
+  }
+
   render() {
     console.log('return React element to build DOM')
-    return <div>Hello</div>
+    return (
+      <div>
+        <div>
+          <button onClick={this.show}>Show</button>
+          <button onClick={this.hide}>Hide</button>
+          <button onClick={this.update}>Update</button>
+        </div>
+        {this.state.visible && <div>{this.state.number}</div>}
+      </div>
+    )
   }
 }
 
