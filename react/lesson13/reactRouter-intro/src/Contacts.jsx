031186c32edeaedae04a7bc10d0d6e9ab47f611d
pampukhaleo <lgpampukha@gmail.com>
2022-11-12 23:10:56 +0200

    react router dom

---
+++ react/lesson13/reactRouter-intro/src/Contacts.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Contacts = () => {
+  return (
+    <div className="page__content">
+      <h1>Contacts</h1>
+      <p>We are here 🗺</p>
+      <a href="/">Go home</a>
+    </div>
+  )
+}
+
+export default Contacts
