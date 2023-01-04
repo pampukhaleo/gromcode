8db69f2efaa9b2585f6da803b2ec0c813a698b0c
pampukhaleo <lgpampukha@gmail.com>
2023-01-04 17:39:15 +0200

    middleware logger

---
+++ redux/lesson5/counterMiddleware/src/Counter.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import * as counterActions from './counter.actions';
+
+const Counter = ({ counter, increment, decrement, reset }) => {
+  return (
+    <div className="counter">
+      <button data-action="decrease" className="counter__button" onClick={decrement}>
+        -
+      </button>
+      <span className="counter__value" onClick={reset}>
+        {counter}
+      </span>
+      <button data-action="increase" className="counter__button" onClick={increment}>
+        +
+      </button>
+    </div>
+  );
+};
+
+const mapState = state => {
+  return {
+    counter: state.counter,
+  };
+};
+
+const mapDispatch = {
+  increment: counterActions.increment,
+  decrement: counterActions.decrement,
+  reset: counterActions.reset,
+};
+
+const connector = connect(mapState, mapDispatch);
+
+const ConnectedCounter = connector(Counter);
+
+export default ConnectedCounter;
